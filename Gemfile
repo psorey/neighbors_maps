96ee9c0fcbb13c70d9ef970b4b8b4e04fd56e5c9
Paul Sorey <psorey@comcast.net>
2011-09-09 14:01:50 -0700

    add bundler

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source "http://rubygems.org"
+
+gem "rails", "2.3.8"
+gem "curb", "0.7.15"
+
+gem "GeoRuby", "1.3.4"
+gem "spatial_adapter", "1.2.0"
+gem "json", "1.5.1"
+gem "open4", "1.0.1"
+gem "pg", "0.10.1"
+gem "Platform", "0.4.0"
+gem "POpen4", "0.1.4"
+
+gem "proj4rb", "0.3.1", :require => "proj4"
+
+
+#gem "rack", "1.1.2"
+
+#gem "rake", "0.8.7"
+
+
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-# RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+  RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 
 # Uncomment below to force Rails into production mode when 
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.active_record.observers = :user_observer
-  config.gem "proj4rb", :lib => "proj4"
+  # config.gem "proj4rb", :lib => "proj4"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
