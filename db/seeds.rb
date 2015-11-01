4a9d3f559fd6595cb3d5cccb47574bed9cc4fd04
Paul Sorey <paul@paulsorey.com>
2015-10-31 20:52:41 -0700

    switching to geoJSON

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-MappedLine.create!([
-  {end_label: "5", data: nil, owner_id: "44", map_layer_id: "walking_paths_survey", geometry: #<RGeo::Geographic::SphericalLineStringImpl:0x318420c "LINESTRING (127.98393814009614 90.0, 141.94723633001558 90.0, -164.5783757099416 90.0, -153.64227051008493 90.0, -152.05477138003334 90.0, -144.11727565992624 90.0, 136.1931008999236 90.0, 170.0597492000088 90.0)">},
-  {end_label: "3", data: nil, owner_id: "44", map_layer_id: "walking_paths_survey", geometry: #<RGeo::Geographic::SphericalLineStringImpl:0x318e2fc "LINESTRING (112.46172429993749 90.0, 95.52840010006912 90.0, -66.00562360999174 90.0, -74.47228569001891 90.0, -2.5056578901130706 90.0, -44.83896836009808 90.0)">},
-  {end_label: "7", data: nil, owner_id: "44", map_layer_id: "walking_paths_survey", geometry: #<RGeo::Geographic::SphericalLineStringImpl:0x3198ba8 "LINESTRING (127.98393814009614 90.0, 129.39504850003868 90.0, -87.00541780004278 90.0, 2.061395199969411 90.0)">},
-  {end_label: "20", data: nil, owner_id: "44", map_layer_id: "walking_paths_survey", geometry: #<RGeo::Geographic::SphericalLineStringImpl:0x319f78c "LINESTRING (-118.47009110008366 90.0, 28.528468699892983 90.0)">},
-  {end_label: "6", data: nil, owner_id: "44", map_layer_id: "walking_paths_survey", geometry: #<RGeo::Geographic::SphericalLineStringImpl:0x319e350 "LINESTRING (161.5930870999582 90.0, 110.79311449988745 90.0)">}
-])
